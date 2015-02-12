fad1b3bba5a7b32615b762ecb4e2025f61cf4d2d
Doffe <doffe_09@hotmail.com>
2015-02-12 13:06:32 +0100

    Dream on baby

---
+++ ProjectExplorer/Source/ProjectExplorer/Door.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ProjectExplorer.h"
+#include "Door.h"
+
+
+
+
---
+++ ProjectExplorer/Source/ProjectExplorer/Door.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Character.h"
+#include "Door.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTEXPLORER_API ADoor : public ACharacter
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
